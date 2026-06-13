707570d65a727ff8d297154bf65e129f05f1be5f
Naman Saini <namansaini464@gmail.com>
2026-06-13 11:08:53 +0530

    Added code to run file or prompt and report error if any

--- exon.java
+++ exon.java
@@ -8,8 +8,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+public class Exon {
+    static boolean hadError = false;
 
-public class Exon{
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.out.println("Usage: Exon [script]");
@@ -20,8 +21,44 @@ public class Exon{
             runPrompt();
         }
     }
-    public static void runFile() {
-        
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if(hadError) System.exit(65);
     }
-    
-} 
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("--> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens)
+            System.out.println(token);
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message)
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+}
