64156ba7c1b94c30ee672bcf00af84f39a9bb5d2
Naman Saini <namansaini464@gmail.com>
2026-07-03 17:34:39 +0530

    Added some interpreter method to Interpreater class

--- com/interpreter/exon/Interpreter.java
+++ com/interpreter/exon/Interpreter.java
@@ -1,5 +1,87 @@
 package com.interpreter.exon;
 
+import com.interpreter.exon.Expr.Binary;
+import com.interpreter.exon.Expr.Grouping;
+import com.interpreter.exon.Expr.Literal;
+import com.interpreter.exon.Expr.Unary;
+
 public class Interpreter implements Expr.Visitor<Object> {
-        
+    @Override
+    public Object visitLiteralExpr(Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Unary expr) {
+        Object right = evaluate(expr.right);
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double) right;
+        }
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
+        return true;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double)
+                    return (double) left + (double) right;
+                if (left instanceof String && right instanceof String)
+                    return (String) left + (String) right;
+                break;
+
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+        }
+        return null;
+    }
+    
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+        return a.equals(b);
+    }
+
 }
